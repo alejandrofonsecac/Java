cadba55735c062a9b8a05c6cd22e4f4e3376bea2
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-10-10 17:00:15 -0300

    Tipos primitivos

---
+++ maratona-java/src/devdojo/introducao-java/Aula02TiposPrimitivos.java
@@ -0,0 +1,18 @@
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long e boolean
+
+        int age = 19;//Inteiro
+        long numeroGrande = 100000;
+        double salarioDouble = 2000D;
+        float salarioFloat = 2000.2F;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+
+        System.out.println("A idade e: " +age+ "anos");
+        System.out.println(salarioFloat);
+    }
+}
--- maratona-java/src/devdojo/introducao-java/DevDojo.java
+++ maratona-java/src/devdojo/introducao-java/DevDojo.java
@@ -1,5 +1,11 @@
 public class DevDojo{
     public static void main(String[] args) {
+        //Comentario
+
+        /* Comentario 
+            com varias
+            linhas
+         */
         System.out.println("Hello World");
     }
 }
