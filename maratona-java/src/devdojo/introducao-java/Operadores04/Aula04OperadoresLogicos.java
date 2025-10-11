fae93b55db7dde2d7f9159b93fcd4849c4f4355a
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-10-11 16:19:03 -0300

    Operadores Logicos

---
+++ maratona-java/src/devdojo/introducao-java/Operadores04/Aula04OperadoresLogicos.java
@@ -0,0 +1,20 @@
+package Operadores04;
+
+public class Aula04OperadoresLogicos {
+    
+    public static void main(String[] args) {
+        
+
+    //AND && (E)
+    //OR || (OU)
+    //NOT ! (negação)
+
+    int idade = 36;
+    float salario = 5900.34F;
+
+    boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+    boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3612;
+    System.out.println(isDentroDaLeiMaiorQueTrinta);
+    System.out.println(isDentroDaLeiMenorQueTrinta);
+    }  
+}
