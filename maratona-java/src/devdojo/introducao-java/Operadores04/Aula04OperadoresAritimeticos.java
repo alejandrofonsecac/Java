7e0ae849150bcd81b2125aea2cb9adec87f3809e
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-10-11 15:47:34 -0300

    organização de pastas

---
+++ maratona-java/src/devdojo/introducao-java/Operadores04/Aula04OperadoresAritimeticos.java
@@ -0,0 +1,21 @@
+package Operadores04;
+public class Aula04OperadoresAritimeticos {
+    public static void main (String[] args){
+        //O resultado de dois numeros int deve ser int!!!
+
+        int n1 = 10;
+        int n2 = 20;
+        int res = n1 * n2;
+        System.out.println(res);
+
+        double n3 = 10;
+        double n4 = 20;
+        double res2 = n3 / n4;
+        System.out.println(res2);
+
+        // Resto (%)
+            //Serve para par e impar
+        int resto = 21 % 3;
+        System.out.println(resto);
+    }
+}
--- maratona-java/src/devdojo/introducao-java/Operadores04/Aula04OperadoresRelacionais.java
+++ maratona-java/src/devdojo/introducao-java/Operadores04/Aula04OperadoresRelacionais.java
@@ -1,23 +1,8 @@
-public class Aula04OperadoresAritimeticos {
-    public static void main (String[] args){
-        //O resultado de dois numeros int deve ser int!!!
+package Operadores04;
 
-        int n1 = 10;
-        int n2 = 20;
-        int res = n1 * n2;
-        System.out.println(res);
+public class Aula04OperadoresRelacionais {
 
-        double n3 = 10;
-        double n4 = 20;
-        double res2 = n3 / n4;
-        System.out.println(res2);
-
-        // Resto (%)
-            //Serve para par e impar
-        int resto = 21 % 3;
-        System.out.println(resto);
-
-        
+    public static void main(String[] args) {
         // <>   <=  >=  ==   !=
             //<> Maior ou menor
             // >= / <= Maior que ou Menor que
