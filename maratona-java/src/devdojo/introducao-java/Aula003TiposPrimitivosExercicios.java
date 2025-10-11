7a5e4b6d6f3038d7012e3e096727de171c2ac5c6
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-10-11 15:07:30 -0300

    Debugando o codigo

--- maratona-java/src/devdojo/introducao-java/Aula003TiposPrimitivosExercicios.java
+++ maratona-java/src/devdojo/introducao-java/Aula003TiposPrimitivosExercicios.java
@@ -1,6 +1,7 @@
 public class Aula003TiposPrimitivosExercicios {
     public static void main(String[] args) {
         String nome = "Jorge";
+        System.out.println(nome);
         String endereco = "Rua Sao Bernardo";
         float salario = 2978.34F;
         String date = "24/07/2027";
