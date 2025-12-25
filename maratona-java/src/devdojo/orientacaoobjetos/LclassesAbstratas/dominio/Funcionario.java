bb5bdf3163a08953d535dfd36d9fa9879e8cf09f
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2025-12-25 15:53:42 -0300

    Classes abstratas

--- maratona-java/src/devdojo/orientacaoobjetos/LclassesAbstratas/dominio/Funcionario.java
+++ maratona-java/src/devdojo/orientacaoobjetos/LclassesAbstratas/dominio/Funcionario.java
@@ -1,11 +1,20 @@
 package devdojo.orientacaoobjetos.LclassesAbstratas.dominio;
 
-public abstract class Funcionario {
+public abstract class Funcionario extends Pessoa {
     protected String nome;
     protected double salario;
 
     public Funcionario(String nome, double salario) {
         this.nome = nome;
         this.salario = salario;
+        calculaBonus();
     }
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo...");
+        // Agora que a classe mae Funcioanrio tem o metodo herdado de PESSOA, gerente e desenvolvedor herdaram esse metodo e nao dara mais erro
+    }
+
+    public abstract void calculaBonus();
 }
---
+++ maratona-java/src/devdojo/orientacaoobjetos/LclassesAbstratas/dominio/Pessoa.java
@@ -0,0 +1,5 @@
+package devdojo.orientacaoobjetos.LclassesAbstratas.dominio;
+
+public abstract class Pessoa {
+    public abstract void imprime();
+}
