d5fd81186e905e99e8bccdd0c7c92b0c62a5ec97
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-01-02 01:25:38 -0300

    Exceção unchecked

---
+++ maratona-java/src/devdojo/orientacaoobjetos/Oexeption/runTime/test/RunTimeExeptionTest02.java
@@ -0,0 +1,15 @@
+package devdojo.orientacaoobjetos.Oexeption.runTime.test;
+
+public class RunTimeExeptionTest02 {
+    public static void main(String[] args) {
+        divisao(1, 0);
+    }
+
+    private static  int divisao(int a, int b){
+        if (b == 0){
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+        }
+        return a/b;
+    }
+
+}
