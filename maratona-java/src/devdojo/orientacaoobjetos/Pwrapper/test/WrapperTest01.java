94228e77423a9e16a396c9bb130a5afeeecc58a6
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-01-07 23:48:37 -0300

    Metodos de Character, Autoboxing e Unboxing

--- maratona-java/src/devdojo/orientacaoobjetos/Pwrapper/test/WrapperTest01.java
+++ maratona-java/src/devdojo/orientacaoobjetos/Pwrapper/test/WrapperTest01.java
@@ -13,11 +13,25 @@ public class WrapperTest01 {
 
         Byte byteW = 1;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; // Autoboxing (conversão automatica de um tipo primitivo para Wrapper )
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 10D;
         Character charW = 'a';
         Boolean booleanW = false;
+
+        int i = intW; // unboxing (tranformar Wrapper para tipo primitivo)
+        Integer intW2 = Integer.parseInt("1");  // Tranformar string em int => Interger
+        boolean verdadeiro = Boolean.parseBoolean("True");
+        System.out.println(verdadeiro);
+
+        System.out.println("--- Testes com o Character ---");
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isDigit('a'));
+        System.out.println(Character.isUpperCase('a'));
+        System.out.println(Character.isLowerCase('v'));
+
+        System.out.println(Character.toUpperCase('a'));
+
     }
 }
