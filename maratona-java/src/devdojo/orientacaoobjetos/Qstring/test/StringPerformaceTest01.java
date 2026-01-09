fed56082c2c13b7796480cf156f8669dd06e52d4
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-01-09 16:34:18 -0300

    String Performace

---
+++ maratona-java/src/devdojo/orientacaoobjetos/Qstring/test/StringPerformaceTest01.java
@@ -0,0 +1,45 @@
+package devdojo.orientacaoobjetos.Qstring.test;
+
+import javafx.beans.binding.StringBinding;
+
+public class StringPerformaceTest01 {
+    public static void main(String[] args) {
+        long inicio = System.currentTimeMillis();
+        concatString(30_000);
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para String: " + (fim - inicio) + " ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuilder(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuilder: " + (fim - inicio) + " ms");
+
+        inicio = System.currentTimeMillis();
+        concatStringBuffer(1_000_000);
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo gasto para StringBuffer: " + (fim - inicio) + " ms");
+    }
+
+
+
+    private static void concatString(int tamanho){
+        String txt = "";
+        for (int i = 0; i < tamanho ; i++) {
+            txt += i;
+        }
+    }
+
+    private static void concatStringBuilder(int tamanho){
+        StringBuilder sb = new StringBuilder(tamanho);
+        for (int i = 0; i < tamanho ; i++) {
+            sb.append(i);
+        }
+    }
+
+    private static void concatStringBuffer(int tamanho){
+        StringBuffer sb = new StringBuffer(tamanho);
+        for (int i = 0; i < tamanho ; i++) {
+            sb.append(i);
+        }
+    }
+}
