16e0c53d14a4c87fe1dd1a41cd539455ee4c93b5
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-04-01 16:25:18 -0300

    Testes em Streams

---
+++ maratona-java/src/SistemaFuncionarios/test/FuncionarioTest02.java
@@ -0,0 +1,42 @@
+package SistemaFuncionarios.test;
+
+import SistemaFuncionarios.domain.Cargos;
+import SistemaFuncionarios.domain.Funcionario;
+import SistemaFuncionarios.repository.FuncionarioRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FuncionarioTest02 {
+    public static void main(String[] args) {
+        FuncionarioRepository funcionarioRepository = new FuncionarioRepository();
+        List<Funcionario> funcionarios = funcionarioRepository.getFuncionarios();
+
+        Map<Cargos, List<Funcionario>> collect = funcionarios.stream()
+                .collect(Collectors.groupingBy(Funcionario::getCargos));
+        System.out.println(collect);
+
+        System.out.println();
+
+        Map<Cargos, Double> mediaSalarialPorCargo = funcionarios.stream()
+                .collect(Collectors.groupingBy(Funcionario::getCargos, Collectors.averagingDouble(Funcionario::getSalario)));
+        System.out.println(mediaSalarialPorCargo);
+
+        System.out.println();
+
+        System.out.println("============");
+        funcionarios.stream()
+                .collect(Collectors.groupingBy(Funcionario::getCargos, Collectors.maxBy(Comparator.comparing(Funcionario::getSalario))))
+                .forEach((cargos, funcionario) ->
+                        funcionario.ifPresent(f -> System.out.println(f)));
+
+        System.out.println();
+
+        System.out.println("Soma dos salarios por cargo");
+        Map<Cargos, Double> collect1 = funcionarios.stream()
+                .collect(Collectors.groupingBy(Funcionario::getCargos, Collectors.summingDouble(Funcionario::getSalario)));
+        System.out.println(collect1);
+
+        collect1.forEach(((cargos, total) -> System.out.println(cargos + " -> " + total)));
+    }
+}
