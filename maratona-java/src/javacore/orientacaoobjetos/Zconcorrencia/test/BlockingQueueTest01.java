d7192380e63ffabeb4a97e63e7621503b39e50b4
alejandrofonsecac <jfonsecacorrea08@gmail.com>
2026-04-05 09:38:16 -0300

    ArrayBlockingQueue

---
+++ maratona-java/src/javacore/orientacaoobjetos/Zconcorrencia/test/BlockingQueueTest01.java
@@ -0,0 +1,39 @@
+package javacore.orientacaoobjetos.Zconcorrencia.test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class BlockingQueueTest01 {
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<String> bq = new ArrayBlockingQueue<>(1);
+        bq.put("Jorge");
+
+        System.out.printf("%s added the named value %s%n", Thread.currentThread().getName(), bq.peek());
+        System.out.println("Trying to add another value");
+        new Thread(new RemoveFromQueue(bq)).start();
+
+        bq.put("Alejandro");
+        System.out.printf("%s added the named value %s%n", Thread.currentThread().getName(),bq.peek());//Ele espera o nome anterior sair
+    }
+
+    static class RemoveFromQueue implements Runnable{
+        private final BlockingQueue<String> bq;
+
+        public RemoveFromQueue(BlockingQueue<String> bq) {
+            this.bq = bq;
+        }
+
+        @Override
+        public void run() {
+            System.out.printf("%s going to sleep for 2s%n", Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                System.out.printf("%s removing value from queue%s%n", Thread.currentThread().getName(), bq.take());
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
